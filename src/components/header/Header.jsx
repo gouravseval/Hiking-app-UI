f7cc8ebf98c2a148a4e0452c07f867f591c3e667
Gourav Jangra <104681594+gouravseval@users.noreply.github.com>
2024-06-16 00:46:16 +0530

    Update Header.jsx
    header update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,14 +6,14 @@ function Header() {
         <nav className="navbar postion-absolute z-2 navbar-expand-lg header navbar-light ">
             <div className="container-fluid z-2 d-flex justify-content-between p-3 gap-5">
             <div style={{ color: "white"}} className="mt-5 z-2 px-4 p-5 d-none fs-5 fw-medium d-lg-flex align-items-end gap-5" >
-            <img style={{width: "calc(8vw + 80px)"}} className='' src="../../../public/assets/logo.png" alt="" />
+            <img style={{width: "calc(8vw + 80px)"}} className='' src="../../../assets/logo.png" alt="" />
                         <a className="nav-link z-2" href="#">Home</a>
                         <a className="nav-link z-2" href="#">Class</a>
                         <a className="nav-link z-2" href="#">Promo</a>
                         <a className="nav-link z-2" href="#">Online Class</a>
                         <a className="nav-link z-2" href="#">Contact</a>
                     </div>
-                <img style={{width: "calc(8vw + 80px)"}} className='d-lg-none z-2' src="../../../public/assets/logo.png" alt="" />
+                <img style={{width: "calc(8vw + 80px)"}} className='d-lg-none z-2' src="../../../assets/logo.png" alt="" />
                 <button style={{width: "calc(2vw + 23px)"}} className="d-lg-none rounded z-2 border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className=" navbar-toggler-icon"></span>
                 </button>
